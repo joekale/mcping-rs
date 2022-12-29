a0fe311553e47e6bc7cd1409574695a0de391ead
Joe Kale <joe.kale1217@gmail.com>
2022-12-29 12:40:55 -0600

    cleaned up varints and added varlong and implemented cidr parsing for scan

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use std::net::{TcpStream, Shutdown, ToSocketAddrs};
+use std::net::{TcpStream, Shutdown, SocketAddr, IpAddr, Ipv4Addr};
+use std::collections::HashSet;
 use std::time::Duration;
 
 use crate::mc_packets::Sendable;
@@ -6,55 +7,212 @@ use std::io::Write;
 use std::io::Read;
 pub mod mc_packets;
 
-fn main() {
-    println!("MineCraft Ping!");
+use clap::Parser;
 
-    let server = String::from("purpleprison.org");
-    let port: u16 = 25565;
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author = "Joe Kale")]
+#[command(version = "0.1.0")] 
+#[command(about = "Scans IP Addresses for MineCraft Servers.", long_about = None)]
+struct Args {
+   /// IP Addresses to include in scan (CIDR Notation). Can be passed multiple times
+   #[arg(short, long, )]
+   include: Vec<String>,
 
-    let addr_str = format!("{}:{}", server, port.to_string());
-    let mut addrs = addr_str.to_socket_addrs().unwrap();
-    let addr = &mut addrs.next().expect("Server did not resolve to Socket Address");
-    println!("Connecting to {}", addr);
-    let mut stream = TcpStream::connect_timeout(addr, Duration::new(10, 0)).unwrap();
-    let _rt_res = stream.set_read_timeout(Some(Duration::new(10, 0)));
-    let mut hsp = mc_packets::Handshake::new();
-    hsp.server_address = server;
+   /// IP Addresses to exclude from scan (CIDR Notation). Can be passed multiple timest
+   #[arg(short, long)]
+   exclude: Vec<String>,
 
-    let hs_bytes = hsp.serialize_to();
-    let write_res = stream.write(hs_bytes.as_slice());
-    if write_res.is_err() {
-        panic!("Failed to write handshake packet: {:?}", write_res.err());
+   #[arg(short, long)]
+   verbose: bool
+}
+
+fn range_from_cidr(cidr_str: &str) -> Result<(u32, u32), &'static str> {
+    let parts: Vec<&str> = cidr_str.split("/").collect();
+    if parts.len() != 2 {
+        return Err("Invalid CIDR notation");
     }
 
-    let status_req_packet = mc_packets::StatusRequest::new();
-    let srq_bytes = status_req_packet.serialize_to();
-    let srq_res = stream.write(&srq_bytes.as_slice());
-    if srq_res.is_err() {
-        panic!("Failed to write StatusRequest Packet: {:?}", srq_res.err());
-    }
+    let ip_str = parts[0];
+    let mask_length_str = parts[1];
 
-    let buf = &mut Vec::<u8>::new();
-    let resp = stream.read_to_end(buf);
-    let read_size = match resp {
-        Ok(read) => read,
-        Err(error) => {
-            panic!("Failed to read_to_end of response: {:?}", error);
-        },
+    let ip = match ip_str.parse::<Ipv4Addr>() {
+        Ok(ip) => ip,
+        Err(_) => {
+            return Err("Invalid IP address");
+        }
     };
 
-    if read_size > 0 {
-        let received = mc_packets::ReceivablePacket::deserialize_from(buf).unwrap();
+    let mask_length = match mask_length_str.parse::<u8>() {
+        Ok(length) => length,
+        Err(_) => {
+            return Err("Invalid mask length");
+        }
+    };
 
-        let json_resp: serde_json::Value = match received {
-            mc_packets::ReceivablePacket::StatusResponse(packet) => packet.status,
-        };
+    let ip_int = u32::from(ip);
+    let mask = !(0xffffffff >> mask_length); // shift down and bitwise not
+    let start = ip_int & mask;
+    let end = start | !mask;
 
-        print!("{}", json_resp);
+    Ok((start, end))
+}
+
+fn merge_include_and_exclude_ranges(includes: &HashSet<(u32,u32)>, excludes: &HashSet<(u32,u32)>) -> HashSet<(u32,u32)> {
+    let mut scan_ranges = HashSet::<(u32,u32)>::new();
+    for range in includes {
+        for erange in excludes {
+            match match ((range.0 ..= range.1).contains(&erange.0), (range.0 ..= range.1).contains(&erange.1)) {
+                (true, false) => {
+                    Some(merge_include_and_exclude_ranges(&vec![(range.0, erange.0 - 1)].into_iter().collect(), &excludes.clone()))
+                },
+                (false, true) => {
+                    Some(merge_include_and_exclude_ranges(&vec![(erange.1 + 1, range.1)].into_iter().collect(), &excludes.clone()))
+                },
+                (true, true) => {
+                    if range == erange {
+                        None
+                    } else if range.0 == erange.0 {
+                        Some(merge_include_and_exclude_ranges(&vec![(erange.1 + 1, range.1)].into_iter().collect(), &excludes.clone()))
+                    } else if range.1 == erange.1 {
+                        Some(merge_include_and_exclude_ranges(&vec![(range.0, erange.0 - 1)].into_iter().collect(), &excludes.clone()))
+                    } else {
+                        Some(merge_include_and_exclude_ranges(& vec![(range.0, erange.0 - 1), (erange.1 + 1, range.1)].into_iter().collect(), &excludes.clone()))
+                    }
+                },
+                _ => None
+            } {
+                Some(x) => {
+                    for range in x {
+                        scan_ranges.insert(range);
+                    }
+                },
+                None => continue
+            };
+        }
+    }
+    if scan_ranges.len() == 0 {
+        includes.clone()
     } else {
-        println!("Received no bytes back from read");
+        scan_ranges
+    }
+}
+
+#[test]
+fn test_merge_simple_high_overlap() {
+    // (192.168.0.0 - 192.168.1.255) excluding (192.168.1.0 - 192.168.1.255) (overlap and subset)
+    assert_eq!(merge_include_and_exclude_ranges(&vec![(u32::from(Ipv4Addr::new(192,168,0,0)), u32::from(Ipv4Addr::new(192,168,1,255)))].into_iter().collect(), 
+                                                &vec![(u32::from(Ipv4Addr::new(192,168,1,0)), u32::from(Ipv4Addr::new(192,168,2,255)))].into_iter().collect()), 
+                                                vec![(u32::from(Ipv4Addr::new(192,168,0,0)), u32::from(Ipv4Addr::new(192,168,0,255)))].into_iter().collect());
+    assert_eq!(merge_include_and_exclude_ranges(&vec![(u32::from(Ipv4Addr::new(192,168,0,0)), u32::from(Ipv4Addr::new(192,168,1,255)))].into_iter().collect(), 
+                                                &vec![(u32::from(Ipv4Addr::new(192,168,1,0)), u32::from(Ipv4Addr::new(192,168,1,255)))].into_iter().collect()), 
+                                                vec![(u32::from(Ipv4Addr::new(192,168,0,0)), u32::from(Ipv4Addr::new(192,168,0,255)))].into_iter().collect());
+}
+
+#[test]
+fn test_merge_simple_low_overlap() {
+    // (192.168.0.0 - 192.168.1.255) excluding (192.168.0.0 - 192.168.0.255) (overlap and subset)
+    assert_eq!(merge_include_and_exclude_ranges(&vec![(u32::from(Ipv4Addr::new(192,168,0,0)), u32::from(Ipv4Addr::new(192,168,1,255)))].into_iter().collect(), 
+                                                &vec![(u32::from(Ipv4Addr::new(192,167,0,0)), u32::from(Ipv4Addr::new(192,168,0,255)))].into_iter().collect()), 
+                                                vec![(u32::from(Ipv4Addr::new(192,168,1,0)), u32::from(Ipv4Addr::new(192,168,1,255)))].into_iter().collect());
+    assert_eq!(merge_include_and_exclude_ranges(&vec![(u32::from(Ipv4Addr::new(192,168,0,0)), u32::from(Ipv4Addr::new(192,168,1,255)))].into_iter().collect(), 
+                                                &vec![(u32::from(Ipv4Addr::new(192,168,0,0)), u32::from(Ipv4Addr::new(192,168,0,255)))].into_iter().collect()), 
+                                                vec![(u32::from(Ipv4Addr::new(192,168,1,0)), u32::from(Ipv4Addr::new(192,168,1,255)))].into_iter().collect());
+}
+
+#[test]
+fn test_merge_multiple_overlap() {
+    // (192.168.0.0 - 192.168.4.255) excluding (192.168.1.0 - 192.168.1.255) and (192.168.3.0 - 192.168.3.255)
+    assert_eq!(merge_include_and_exclude_ranges(&vec![(u32::from(Ipv4Addr::new(192,168,0,0)), u32::from(Ipv4Addr::new(192,168,4,255)))].into_iter().collect(), 
+                                                &vec![(u32::from(Ipv4Addr::new(192,168,1,0)), u32::from(Ipv4Addr::new(192,168,1,255))),
+                                                (u32::from(Ipv4Addr::new(192,168,3,0)), u32::from(Ipv4Addr::new(192,168,3,255)))].into_iter().collect()), 
+                                                vec![(u32::from(Ipv4Addr::new(192,168,0,0)), u32::from(Ipv4Addr::new(192,168,0,255))),
+                                                (u32::from(Ipv4Addr::new(192,168,2,0)), u32::from(Ipv4Addr::new(192,168,2,255))),
+                                                (u32::from(Ipv4Addr::new(192,168,4,0)), u32::from(Ipv4Addr::new(192,168,4,255)))].into_iter().collect());
+}
+
+fn main() {
+    let app_cli = Args::parse();
+    println!("MineCraft Ping!");
+
+    println!("include: {:#?}", app_cli.include);
+    println!("exclude: {:#?}", app_cli.exclude);
+
+    let mut include_ranges = HashSet::<(u32, u32)>::new();
+    for cidr_str in &app_cli.include {
+        include_ranges.insert(range_from_cidr(cidr_str).unwrap());
     }
 
-    stream.shutdown(Shutdown::Both).unwrap();
+    let mut exclude_ranges = HashSet::<(u32, u32)>::new();
+    for cidr_str in &app_cli.exclude {
+        exclude_ranges.insert(range_from_cidr(cidr_str).unwrap());
+    }
+
+    let scan_ranges = merge_include_and_exclude_ranges(&include_ranges, &exclude_ranges);
+    let port: u16 = 25565;
+
+    for range in scan_ranges {
+        for ip in range.0 ..= range.1 {
+            let addr = SocketAddr::new(IpAddr::V4(Ipv4Addr::from(ip)), port);
+            let mut stream = match TcpStream::connect_timeout(&addr, Duration::new(1, 0)) {
+                Ok(x) => x,
+                Err(err) => {
+                    if app_cli.verbose {
+                        println!("Failed to connect to {}: {}", addr.ip().to_string(), err);
+                    }
+                    continue
+                }
+            };
+            let _ = stream.set_read_timeout(Some(Duration::new(1, 0)));
+
+            let mut hsp = mc_packets::Handshake::new();
+            hsp.server_address = addr.ip().to_string();
+
+            let hs_bytes = hsp.serialize_to();
+            if stream.write(hs_bytes.as_slice()).is_err() {
+                if app_cli.verbose {
+                    println!("Connected to socket, but writing handshake failed.");
+                }
+                stream.shutdown(Shutdown::Both).unwrap();
+                continue
+            }
+
+            let status_req_packet = mc_packets::StatusRequest::new();
+            let srq_bytes = status_req_packet.serialize_to();
+            if stream.write(&srq_bytes.as_slice()).is_err() {
+                if app_cli.verbose {
+                    println!("Connected to socket, but writing StatusRequest failed.");
+                }
+                stream.shutdown(Shutdown::Both).unwrap();
+                continue
+            }
+
+            let buf = &mut Vec::<u8>::new();
+            let resp = stream.read_to_end(buf);
+            let read_size = match resp {
+                Ok(read) => read,
+                Err(error) => {
+                    if app_cli.verbose {
+                        println!("Connected to socket, but reading response failed: {}", error);
+                    }
+                    stream.shutdown(Shutdown::Both).unwrap();
+                    continue
+                },
+            };
+
+            if read_size > 0 {
+                let received = mc_packets::ReceivablePacket::deserialize_from(buf).unwrap();
+
+                let json_resp: serde_json::Value = match received {
+                    mc_packets::ReceivablePacket::StatusResponse(packet) => packet.status,
+                };
+
+                print!("{}", json_resp);
+            } else {
+                println!("Received no bytes back from read");
+            }
+
+        }
+    }
     
 }
--- src/mc_packets.rs
+++ src/mc_packets.rs
@@ -1,12 +1,13 @@
 type VarInt = i32;
+type VarLong = i64;
 
 trait VarIntExt {
-    fn append_as_varint(&self, vector: &mut Vec<u8>) -> ();
-    fn from_varint<'a, I: Iterator<Item = &'a u8>>(iter: &mut I) -> Self;
+    fn append_varint_as_bytes(&self, vector: &mut Vec<u8>) -> ();
+    fn from_varint_bytes<'a, I: Iterator<Item = &'a u8>>(iter: &mut I) -> Self;
 }
 
 impl VarIntExt for i32 {
-    fn append_as_varint(&self, vector: &mut Vec<u8>) {
+    fn append_varint_as_bytes(&self, vector: &mut Vec<u8>) {
         let mut tmp = *self as u32;
         loop {
             if tmp & (!0x0000007F) == 0 {
@@ -19,7 +20,7 @@ impl VarIntExt for i32 {
         }
     }
 
-    fn from_varint<'a, I: Iterator<Item = &'a u8>>(iter: &mut I) -> VarInt {
+    fn from_varint_bytes<'a, I: Iterator<Item = &'a u8>>(iter: &mut I) -> VarInt {
         let mut ret: VarInt = 0;
         let mut pos: i32 = 0;
         loop {
@@ -31,13 +32,50 @@ impl VarIntExt for i32 {
     
             pos += 7;
             if pos >= 32 {
-                
+                panic!("VarInt is too big!");
             }
         }
         ret
     }
 }
 
+trait VarLongExt {
+    fn append_varlong_as_bytes(&self, vector: &mut Vec<u8>) -> ();
+    fn from_varlong_bytes<'a, I: Iterator<Item = &'a u8>>(iter: &mut I) -> Self;
+}
+
+impl VarLongExt for i64 {
+    fn append_varlong_as_bytes(&self, vector: &mut Vec<u8>) {
+        let mut tmp = *self as u64;
+        loop {
+            if tmp & (!0x7Fu64) == 0 {
+                vector.push((tmp & 0xFF) as u8);
+                break;
+            }
+    
+            vector.push((((tmp & 0x7f) | 0x80) & 0xFF) as u8);
+            tmp = tmp >> 7;
+        }
+    }
+
+    fn from_varlong_bytes<'a, I: Iterator<Item = &'a u8>>(iter: &mut I) -> Self {
+        let mut ret: VarLong = 0;
+        let mut pos: i64 = 0;
+        loop {
+            let current = iter.next().expect("VarInt shorter than expected");
+            ret |= ((current & 0x7F) as i64) << pos;
+            if (current & 0x80) == 0 {
+                break;
+            }
+    
+            pos += 7;
+            if pos >= 64 {
+                panic!("VarLong is too big!");
+            }
+        }
+        ret
+    }
+}
 
 
 pub trait Sendable {
@@ -59,26 +97,26 @@ impl Handshake {
             protocol_version: -1,
             server_address: "localhost".to_string(), 
             server_port: 25565, 
-            next_state: 1}
+            next_state: 1
+        }
     }
 }
 
 impl Sendable for Handshake {
     fn serialize_to(&self) -> Vec<u8> {
         let mut packet_data: Vec<u8> = Vec::new();
-        self.id.append_as_varint(&mut packet_data);
-        self.protocol_version.append_as_varint(&mut packet_data);
+        self.id.append_varint_as_bytes(&mut packet_data);
+        self.protocol_version.append_varint_as_bytes(&mut packet_data);
         let string_length:VarInt = self.server_address.len().try_into().unwrap();
-        string_length.append_as_varint(&mut packet_data);
+        string_length.append_varint_as_bytes(&mut packet_data);
         let addr_bytes = &mut self.server_address.clone().into_bytes().to_vec();
         packet_data.append(addr_bytes);
         let port_bytes = &mut self.server_port.to_be_bytes().to_vec();
         packet_data.append(port_bytes);
-        self.next_state.append_as_varint(&mut packet_data);
-
+        self.next_state.append_varint_as_bytes(&mut packet_data);
         let packet_len: VarInt = packet_data.len().try_into().unwrap();
         let mut packet = Vec::<u8>::new();
-        packet_len.append_as_varint(&mut packet);
+        packet_len.append_varint_as_bytes(&mut packet);
         packet.append(&mut packet_data);
         return packet;
     }
@@ -110,14 +148,14 @@ pub enum ReceivablePacket {
 impl ReceivablePacket {
     pub fn deserialize_from(resp: &Vec<u8>) -> Result<ReceivablePacket, &'static str> {
         let mut it = resp.iter();
-        let _length = VarInt::from_varint(&mut it);
-        let id = VarInt::from_varint(&mut it);
+        let _length = VarInt::from_varint_bytes(&mut it);
+        let id = VarInt::from_varint_bytes(&mut it);
         match id {
             0x00 => {
-                let length = VarInt::from_varint(&mut it);
-                let status = serde_json::from_str(&String::from_utf8(it.take(length as usize)
+                let length = VarInt::from_varint_bytes(&mut it);
+                let status = serde_json::from_slice(&it.take(length as usize)
                                                                                     .map(|value| *value)
-                                                                                    .collect::<Vec<u8>>()).unwrap().replace(|chr: char| !chr.is_ascii_graphic() && !chr.is_whitespace(), "")).unwrap();
+                                                                                    .collect::<Vec<u8>>()[..]).unwrap();
                 //return Ok(ReceivablePacket::StatusResponse {0: StatusResponse { status: status}});
                 return Ok(ReceivablePacket::StatusResponse (StatusResponse {status: status }));
             },
